de16fea01c4e8624bf11fa89dcc857cdfa2ffe14
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2024-01-10 16:55:06 +0200

    styles.scss

--- HealthCheck/src/app/app.module.ts
+++ HealthCheck/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule  } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
 import { HealthCheckComponent } from './components/health-check/health-check.component';
- 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,13 @@ import { HealthCheckComponent } from './components/health-check/health-check.com
     HealthCheckComponent
   ],
   imports: [
-    BrowserModule, 
-    HttpClientModule, 
-    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule, BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [
     provideClientHydration()
